abbb69f1e5398b6536db4f313da922ded52d00e1
DaniDoom35 <chargerpod@gmail.com>
2023-02-22 18:08:07 -0600

    Constructor y encapsulacion
    Practica 10

--- Personaje.py
+++ Personaje.py
@@ -1,27 +1,38 @@
 class Personaje:
     
-    #Atributos
-    especie = "Humano"
-    nombre = "Marcus Fenix"
-    altura = "2.70"
-    altura = "2.70"
+    #Definimos el constructor de personaje
+    def __init__(self,esp,nom,alt): 
+        
+        #Atributos
+         self.especie = esp   #Inicializacion de atributos 
+         self.nombre = nom   #Inicializacion de atributos
+         self.altura = alt   #Inicializacion de atributos
+         
+        
+        
+    
+        
+    
+    
     
     
     #Metodos del personaje
     
     def correr(self,status):
         if(status):
-            print("El personaje" + self.nombre + "Esta corriendo")
+            print("El personaje " + self.nombre + "Esta corriendo")
             
         else:
-            print("El personaje" + self.nombre + "Se detuvo")
+            print("El personaje " + self.nombre + "Se detuvo")
             
             
     def lanzarGranadas(self):
-        print("El personaje" + self.nombre + "Utilizo una granada")
+        print("El personaje " + self.nombre + "Utilizo una granada")
         
-    def recargaArma(self):
-        print("El personaje" + self.nombre + "lanzo una granada")
+    def recargaArma(self,ammo):
+        print("El personaje " + self.nombre + "a recargado " + str(ammo) + " de municion")
+        
+        
         
         
         
--- objetos.py
+++ objetos.py
@@ -1,18 +1,55 @@
 from Personaje import *
 
-#!.Crear objeto de la clase Personaje
+#1. Solicitar Datos Heroe
 
-heroe = Personaje()
+print("")
+print("####### Datos Heroe ####")
+especieH = input("Escribe la especie del Heroe: ")
+nombreH = input("Escribe el nombre del Heroe: ")
+alturaH = float(input("Escribe la altura del Heroe: "))
+recargarH = int(input("Cuantas Balas recargas al Heroe: "))
 
-#2. usar atributos
+#1.2. Solicitar Datos Villano
 
+print("")
+print("####### Datos Villano ####")
+especieV = input("Escribe la especie del Villano: ")
+nombreV = input("Escribe el nombre del Villano: ")
+alturaV = float(input("Escribe la altura del Villano: "))
+recargarV = int(input("Cuantas Balas recargas al Villano: "))
+
+
+#2. Crar objeto de clase Personaje
+
+heroe = Personaje(especieH,nombreH,alturaH)
+villano = Personaje(especieV,nombreV,alturaV)
+
+
+
+#3. usar atributos
+
+
+print("")
+print("####### Objeto Heroe ####")
 print("El personaje se llama: " + heroe.nombre)
 print("Pertenece a la especie: " + heroe.especie)
-print("Y tiene una altura de: " + heroe.altura)
+print("Y tiene una altura de: " + str(heroe.altura))
+
+
+print("")
+print("####### Objeto Villano ####")
+print("El personaje se llama: " + villano.nombre)
+print("Pertenece a la especie:asd " + villano.especie)
+print("Y tiene una altura de: " + str(villano.altura))
+
 
 #3. Usar metodos
 
-heroe.correr(true)
+heroe.correr(True)
 heroe.lanzarGranadas()
-heroe.recargaArmaI(87)
+heroe.recargaArma(recargarH)
+
+villano.correr(False)
+villano.lanzarGranadas()
+villano.recargaArma(recargarV)
 
