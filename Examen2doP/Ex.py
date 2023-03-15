acb1a7c7a4d5dc8f2d6bbb255d41e16a7deef0d1
DaniDoom35 <chargerpod@gmail.com>
2023-03-15 16:33:16 -0600

    Examen
    Mi progreso

---
+++ Examen2doP/Ex.py
@@ -0,0 +1,23 @@
+from tkinter import messagebox
+
+
+class Registro ():
+    
+    
+    def __init__(self):
+        
+        pass
+
+        
+        
+        
+    def agregar (self,miEntry,miEntry2,miEntry3,miEntry4,miEntry5):
+        
+        
+     
+     
+     messagebox(text=miEntry)
+
+    
+
+        
---
+++ Examen2doP/interfaz.py
@@ -0,0 +1,90 @@
+
+from tkinter import *
+from Ex import *
+
+val = Registro()
+
+def mandar ():
+    
+    val.agregar(miEntry.get(),miEntry2.get(),miEntry3.get(),miEntry4.get(),miEntry5.get())
+
+
+
+#crear ventana
+
+ventana = Tk()
+
+ventana.title("Examen")
+
+
+#crear un frame
+
+miFrame = Frame(ventana, width=1200, height=600)
+
+
+#agregar , label entry y botones
+
+miLabel = Label(miFrame, text="Nombre")
+
+miLabel.place(x=100, y=100)
+
+miLabel2 = Label(miFrame, text="Apellido Paterno")
+
+miLabel2.place(x=100, y=150)
+
+miLabel3 = Label(miFrame, text="Apellido Materno")
+
+miLabel3.place(x=100, y=200)
+
+miLabel4 = Label(miFrame, text="Año de nacimiento")
+
+miLabel4.place(x=100, y=250)
+
+miLabel5 = Label(miFrame, text="Carrera")
+
+miLabel5.place(x=100, y=300)
+
+
+
+
+#entry
+
+miEntry = Entry(miFrame)
+
+miEntry.place(x=200, y=100)
+
+miEntry2 = Entry(miFrame)
+
+miEntry2.place(x=200, y=150)
+
+miEntry3 = Entry(miFrame)
+
+miEntry3.place(x=200, y=200)
+
+miEntry4 = Entry(miFrame)
+
+miEntry4.place(x=200, y=250)
+
+miEntry5 = Entry(miFrame)
+
+miEntry5.place(x=200, y=300)
+
+
+
+#botones
+
+miBoton = Button(miFrame, text="Enviar",command=mandar)
+
+miBoton.place(x=100, y=400)
+
+
+#agregar frame a la ventana
+
+
+
+miFrame.pack()
+
+
+
+ventana.mainloop()
+
