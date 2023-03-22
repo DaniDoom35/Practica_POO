e0a923de29549cca47e87df1c5523b7326a7eea5
DaniDoom35 <chargerpod@gmail.com>
2023-03-22 17:20:23 -0600

    tkintter pestañas
    Creamos las pestañas y instalamos bcrypt

---
+++ P3/TKINTER_SQLite/UsuariosCRUD.py
@@ -0,0 +1,49 @@
+
+
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+
+#Ventana
+Ventana = Tk()
+Ventana.title("CRUD Usuarios")
+Ventana.geometry("500x300")
+
+#Notebook
+
+panel = ttk.Notebook(Ventana)
+panel.pack(fill="both",expand="yes")
+
+#pestañas
+
+pestana1= ttk.Frame(panel)
+pestana2= ttk.Frame(panel)
+pestana3= ttk.Frame(panel)
+pestana4= ttk.Frame(panel)
+
+#PEstaña1: Formulario Usuarios
+titulo = Label(pestana1,text="Registro de usuarios",fg="Blue",font=("Comic Sans",18)).pack()
+
+varNom= tk.StringVar()
+lblNom= Label(pestana1,text="Nombre: ").pack()
+textNom= Entry(pestana1,textvariable=varNom).pack()
+
+varCor= tk.StringVar()
+lblCor= Label(pestana1,text="Correo: ").pack()
+textCor= Entry(pestana1,textvariable=varCor).pack()
+
+varCon= tk.StringVar()
+lblCon= Label(pestana1,text="Contraseña: ").pack()
+textCon= Entry(pestana1,textvariable=varCon).pack()
+
+btnGuardar= Button(pestana1,text="Guardar Usuario").pack()
+
+
+panel.add(pestana1,text="Formulario de Usuarios")
+panel.add(pestana2,text="Buscar Usuario")
+panel.add(pestana3,text="Consultar Usuarios")
+panel.add(pestana4,text="Actualizar Usuario")
+
+Ventana.mainloop()
+
